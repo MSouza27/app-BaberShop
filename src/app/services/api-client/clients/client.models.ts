fe3f931ce31996b0bd253b1134b1abf240c26609
magnoclear <magnoapt@gmail.com>
2025-02-23 22:36:12 -0300

    Client models

---
+++ src/app/services/api-client/clients/client.models.ts
@@ -0,0 +1,41 @@
+import { Response } from './../../../../../node_modules/@types/express/node_modules/@types/express-serve-static-core/index.d';
+export interface SaveClientRequest {
+  name: string
+  email: string
+  phone: string
+}
+
+export interface UpdateClientRequest {
+  name: string
+  email: string
+  phone: string
+}
+
+export interface SaveClientResponse {
+  id: number
+  name: string
+  email: string
+  phone: string
+}
+
+export interface ResponseClientResponse {
+  id: number
+  name: string
+  email: string
+  phone: string
+}
+
+export interface ListClientResponse {
+  id: number
+  name: string
+  email: string
+  phone: string
+}
+
+export interface DatailClientResponse {
+  id: number
+  name: string
+  email: string
+  phone: string
+}
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const envinronment = {
+  production: false,
+  apiUrl: 'http://localhost:4200/'
+}
